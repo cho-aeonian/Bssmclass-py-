6c50be776077a5d3fc6daf36843096e14a15880a
cho-aeonian <bagchoyeong802@gmail.com>
2023-09-27 23:39:46 +0900

    Bssm_class(py)

---
+++ 0918(나영T).py
@@ -0,0 +1,4 @@
+def profile(name, age, language):
+        print("이름: {0}\t 나이:{1}\t 주사용언어:{2}".format(name,age,language))
+              
+profile("이나영",20,"파이썬")
---
+++ 0918-1(나영T).py
@@ -0,0 +1,8 @@
+def profile(name, age, language):
+        print("이름: {0}\t 나이:{1}\t 주사용언어:{2}".format(name,age,language))
+
+profile("이나영")
+def sum(*a):
+      print(type(a))
+      return max(a)
+print(sum(1,2,3,4,5))
